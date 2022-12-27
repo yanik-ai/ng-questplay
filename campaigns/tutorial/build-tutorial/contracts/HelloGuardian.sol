83f44bfd9869ad4c42a659ffeeee733adce2ffe4
yanik <yanikkoval@gmail.com>
2022-12-27 14:50:33 +0700

    #build-tutorial

--- campaigns/tutorial/build-tutorial/contracts/HelloGuardian.sol
+++ campaigns/tutorial/build-tutorial/contracts/HelloGuardian.sol
@@ -4,6 +4,6 @@ pragma solidity ^0.8.16;
 contract HelloGuardian {
     /// @return "Hello Guardian"
     function hello() external pure returns (string memory) {
-        // return "Hello Guardian";
+        return "Hello Guardian";
     }
 }
